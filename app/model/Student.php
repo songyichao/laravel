bf8ff325cc3963838af945ec5e6cda4dd919d02f
songyichao <songyichao@mioji.com>
2016-12-11 23:19:18 +0800

    qd

--- app/model/Student.php
+++ app/model/Student.php
@@ -13,9 +13,36 @@ class Student extends Model
 	 * 查找学生
 	 * @return mixed
 	 */
+	//	public function getStudent()
+	//	{
+	//		return DB::select('select * from student');
+	//	}
 	public function getStudent()
 	{
-		return DB::select('select * from student');
+		//		return DB::table('student')->get();
+		//		return DB::table('student')
+		//			->orderBy('id', 'desc')
+		//			->first();
+		//		return DB::table('student')
+		//			->whereRaw('id > ? and age = ?', [2, 11])
+		//			->get();
+		//		return DB::table('student')
+		//			->whereRaw('id > ? and sex = ?', [2, 'nv'])
+		//			->pluck('name');
+		//5.3废弃了lists
+		//		return DB::table('student')
+		//			->whereRaw('id > ? and sex = ?', [2, 'nv'])
+		//			->pluck('name', 'id');
+		//		return DB::table('student')
+		//			->select(['id', 'name', 'sex'])
+		//			->whereRaw('id > ? and sex = ?', [2, 'nv'])
+		//			->get();
+		echo "<pre>";
+		DB::table('student')
+			->chunk(2, function ($student) {
+				var_dump($student);
+				return false;
+			});
 	}
 	
 	/**
@@ -25,25 +52,25 @@ class Student extends Model
 	 * @param $sex
 	 * @return mixed
 	 */
-//	public function addStudent($name, $age, $sex)
-//	{
-//		return DB::insert('insert into student (name, age, sex, ctime, utime) VALUE
-//			(?, ?, ?, ?, ?)', [$name, $age, $sex, time(), time()]);
-//	}
+	//	public function addStudent($name, $age, $sex)
+	//	{
+	//		return DB::insert('insert into student (name, age, sex, ctime, utime) VALUE
+	//			(?, ?, ?, ?, ?)', [$name, $age, $sex, time(), time()]);
+	//	}
 	public function addStudent($name, $age, $sex)
 	{
-//		return DB::table('student')->insert(
-//			[
-//				'name'=> $name,
-//				'age' => $age,
-//				'sex' => $sex,
-//				'ctime' => time(),
-//				'utime' => time(),
-//			]
-//		);
+		//		return DB::table('student')->insert(
+		//			[
+		//				'name'=> $name,
+		//				'age' => $age,
+		//				'sex' => $sex,
+		//				'ctime' => time(),
+		//				'utime' => time(),
+		//			]
+		//		);
 		return DB::table('student')->insertGetId(
 			[
-				'name'=> $name,
+				'name' => $name,
 				'age' => $age,
 				'sex' => $sex,
 				'ctime' => time(),
@@ -60,17 +87,36 @@ class Student extends Model
 	 * @param $id
 	 * @return mixed
 	 */
+	//	public function updateStudent($name, $age, $sex, $id)
+	//	{
+	//		return DB::update('update student set name = ? , age = ? , sex = ?
+	// 			, utime = ? WHERE id = ?',
+	//			[$name, intval($age), $sex, time(), intval($id)]);
+	//	}
 	public function updateStudent($name, $age, $sex, $id)
 	{
-		return DB::update('update student set name = ? , age = ? , sex = ?
- 			, utime = ? WHERE id = ?',
-			[$name, intval($age), $sex, time(), intval($id)]);
+		return DB::table('student')
+			->where('id', $id)
+			->update(
+				[
+					'name' => $name,
+					'age' => $age,
+					'sex' => $sex,
+					'utime' => time()
+				]
+			);
 	}
 	
+	//	public function deleteStudent($id)
+	//	{
+	//		return DB::delete('delete from student where id = ?',
+	//			[$id]);
+	//	}
 	public function deleteStudent($id)
 	{
-		return DB::delete('delete from student where id = ?',
-			[$id]);
+		return DB::table('student')
+			->where('id', $id)
+			->delete();
 	}
 	
 }
