8cb2cc8fcde83bbab18a5ee26a7d81e7f3e5d0f3
songyichao <songyichao74@gmail.com>
2017-03-28 22:28:22 +0800

    fix phpunit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Tests\Feature;
+
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -7,11 +10,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testExample()
     {
         $this->get('/')
             ->assertSee('Laravel');
